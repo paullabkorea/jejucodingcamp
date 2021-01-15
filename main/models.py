77119f4934e57de6a7fbc4c48f239ef157be1108
cool-hammer <ajtwlstmdgks@gmail.com>
2021-01-15 15:06:07 +0000

    lat lng

--- main/models.py
+++ main/models.py
@@ -11,16 +11,18 @@ class Cafe(models.Model):
         ('Jeju-si', '제주시'), 
         ('Jocheon-eup', '조천읍'), 
         ('Gujwa-eup', '구좌읍'),
-        ('Daejeong-eup', '대정읍'),
-        ('Andeok-myeon', '안덕면'),
-        ('Seogwipo-si', '서귀포시'),
-        ('Namwon-eup', '남원읍'),
-        ('Pyoseon-myeon', '표선면'),
-        ('Seongsan-eup', '성산읍'),
         ('Udo-myeon', '우도면'),
+        ('Seongsan-eup', '성산읍'),
+        ('Pyoseon-myeon', '표선면'),
+        ('Namwon-eup', '남원읍'),
+        ('Seogwipo-si', '서귀포시'),
+        ('Andeok-myeon', '안덕면'),
+        ('Daejeong-eup', '대정읍'),
     ]
     
     location = models.CharField(max_length=50, choices=locations)
+    lat = models.FloatField(null =True)
+    lng = models.FloatField(null=True)
     mainphoto = models.ImageField(blank=True, null=True)
     published_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
