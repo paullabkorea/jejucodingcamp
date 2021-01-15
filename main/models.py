0cba465788e94a7531dedc84ea3edcc1da5549f2
cool-hammer <ajtwlstmdgks@gmail.com>
2021-01-15 17:03:33 +0900

    cafe location added

--- main/models.py
+++ main/models.py
@@ -3,6 +3,24 @@ from django.db import models
 
 class Cafe(models.Model):
     name = models.CharField(max_length=50)
+    
+    locations = [
+        ('Hangyeong-myeon', '한경면'), 
+        ('Hallim-eup', '한림읍'),
+        ('Aewol-eup', '애월읍'), 
+        ('Jeju-si', '제주시'), 
+        ('Jocheon-eup', '조천읍'), 
+        ('Gujwa-eup', '구좌읍'),
+        ('Udo-myeon', '우도면'),
+        ('Seongsan-eup', '성산읍'),
+        ('Pyoseon-myeon', '표선면'),
+        ('Namwon-eup', '남원읍'),
+        ('Seogwipo-si', '서귀포시'),
+        ('Andeok-myeon', '안덕면'),
+        ('Daejeong-eup', '대정읍'),
+    ]
+    
+    location = models.CharField(max_length=50, choices=locations)
     mainphoto = models.ImageField(blank=True, null=True)
     published_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
