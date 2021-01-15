6d126b3e4ad761c88a03c6c1b67a2797ddb80f43
cool-hammer <ajtwlstmdgks@gmail.com>
2021-01-15 07:34:47 +0000

    published_date & modified_date added

--- main/models.py
+++ main/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Cafe(models.Model):
     name = models.CharField(max_length=50)
     mainphoto = models.ImageField(blank=True, null=True)
-    publishedDate = models.DateTimeField(blank=True, null=True, auto_now_add=True)
-    modifiedDate = models.DateTimeField(blank=True, null=True, auto_now=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
     content = models.TextField()
     
     def __str__(self):
