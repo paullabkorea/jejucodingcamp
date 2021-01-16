0aa030461bd458056702246a88675782dfcfeff5
cool-hammer <ajtwlstmdgks@gmail.com>
2021-01-16 21:27:36 +0900

    location filter

--- main/views.py
+++ main/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render
 from .models import Cafe
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'locations' : Cafe.locations
+    }
+    return render(request, 'main/index.html', context)
 
 
 def cafelist(request):
-    cafes = Cafe.objects.all()
+    selected_locations = request.GET.get('locations')
+    
+    if selected_locations:
+        cafes = Cafe.objects.filter(location__in=selected_locations)
+    else:
+        cafes = Cafe.objects.all()
     
     context = {
         'cafes': cafes
     }
+    
     return render(request, 'main/cafelist.html', {'cafes': cafes})
 
 def cafedetails(request, pk):
